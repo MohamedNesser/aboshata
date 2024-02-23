25ecb0f753b8a3c91470f802907f3aa0e74d7980
shopapp <https://github.com/mohamed11242212/shopapp.git>
2024-02-23 21:47:16 +0200

    first commit

--- lib/presantion/screens/add_person/form_add_person.dart
+++ lib/presantion/screens/add_person/form_add_person.dart
@@ -7,71 +7,74 @@ class FormAddPersone extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
-        child: Column(
-            //   children: [
-            //     CuctomTextField(
-            //       icon: Icon(Icons.person, color: Colors.blue, size: 30),
-            //       labeltext: 'Enter name lost person',
-            //        controller: null,
-            //     ),
-            //     SizedBox(
-            //       height: 10,
-            //     ),
-            //     CuctomTextField(
-            //       icon:
-            //           Icon(Icons.location_on_outlined, color: Colors.blue, size: 30),
-            //       labeltext: 'Enter Address lost person ',
-            //       controller: null,
-            //     ),
-            //     SizedBox(
-            //       height: 10,
-            //     ),
-            //     CuctomTextField(
-            //       icon: Icon(Icons.email_rounded, color: Colors.blue, size: 30),
-            //       labeltext: 'Enter contact mail',
-            //        controller: null,
+    TextEditingController namecontroller = TextEditingController();
+    TextEditingController Addresscontroller = TextEditingController();
+    TextEditingController emailcontroller = TextEditingController();
+    TextEditingController phonecontroller = TextEditingController();
 
-            //     ),
-            //     SizedBox(
-            //       height: 10,
-            //     ),
-            //     CuctomTextField(
-            //       icon: Icon(Icons.phone, color: Colors.blue, size: 40),
-            //       labeltext: 'Enter contact phone',
-            //       controller: null,
-            //     ),
-            //     SizedBox(
-            //       height: 10,
-            //     ),
-            //     SizedBox(
-            //       width: 200.w,
-            //       child: MaterialButton(
-            //         onPressed: () {},
-            //         elevation: 10,
-            //         shape: RoundedRectangleBorder(
-            //             borderRadius: BorderRadius.circular(20)),
-            //         color: Color.fromARGB(255, 37, 224, 209),
-            //         child: Padding(
-            //           padding: EdgeInsets.symmetric(vertical: 10),
-            //           child: Row(
-            //             mainAxisAlignment: MainAxisAlignment.center,
-            //             children: [
-            //               Text(
-            //                 "Confirm infromation ",
-            //                 style: TextStyle(
-            //                     color: Colors.black,
-            //                     fontSize: 15.sp,
-            //                     fontWeight: FontWeight.bold),
-            //               )
-            //             ],
-            //           ),
-            //         ),
-            //       ),
-            //     ),
-            //   ],
-            // ),
-
-            ));
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          CuctomTextField(
+            icon: Icon(Icons.person, color: Colors.blue, size: 30),
+            labeltext: 'Enter name lost person',
+            controller: namecontroller,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          CuctomTextField(
+            icon:
+                Icon(Icons.location_on_outlined, color: Colors.blue, size: 30),
+            labeltext: 'Enter Address lost person ',
+            controller: Addresscontroller,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          CuctomTextField(
+            icon: Icon(Icons.email_rounded, color: Colors.blue, size: 30),
+            labeltext: 'Enter contact email',
+            controller: emailcontroller,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          CuctomTextField(
+            icon: Icon(Icons.phone, color: Colors.blue, size: 40),
+            labeltext: 'Enter contact phone',
+            controller: phonecontroller,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          SizedBox(
+            width: 200.w,
+            child: MaterialButton(
+              onPressed: () {},
+              elevation: 10,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)),
+              color: Color.fromARGB(255, 37, 224, 209),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Confirm infromation ",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15.sp,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
